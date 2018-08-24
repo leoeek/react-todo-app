c147c1770bcb8139164a2feede702ff802fbf91b
Leonardo <leonardolp@gmail.com>
2018-08-24 18:03:51 -0300

    Inicial

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Tarefas" small="Sobre"></PageHeader>
+
+        <h2>Nossa história</h2>
+        <p>Lorem ipsum</p>
+        <h2>Nossa visão</h2>
+        <p>Lorem ipsum</p>
+    </div>
+)
